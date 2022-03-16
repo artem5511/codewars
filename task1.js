1a4109a3fc8df962814f29f70b4122bd737cb1c3
lui viton <artem_5511@mail.ru>
2022-03-17 00:00:07 +0300

    Opposites Attract

--- task1.js
+++ task1.js
@@ -83,3 +83,17 @@ function powersOfTwo(n){
 
 const result = powersOfTwo(2)
 console.log(result)
+
+//Opposites Attract
+function lovefunc(flower1, flower2){
+  // moment of truth
+  if(flower1 % 2 == 0 && flower2 % 2 == 1){
+    return true;
+}
+ if(flower1 % 2 == 1 && flower2 % 2 == 0){
+   return true;
+}
+else{
+    return false;
+    }
+}
