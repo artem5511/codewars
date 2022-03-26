4bffa17e72f7004fd6a854a708b5a32065ff27ce
lui viton <artem_5511@mail.ru>
2022-03-26 12:50:33 +0300

    Update task1.js
    Merge two sorted arrays into one

--- task1.js
+++ task1.js
@@ -97,3 +97,16 @@ else{
     return false;
     }
 }
+
+//Merge two sorted arrays into one
+function mergeArrays(arr1, arr2) {
+
+  let arr = arr1.concat(arr2).sort((a, b) => a - b);
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i + 1]) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
