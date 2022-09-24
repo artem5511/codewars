f48825bcc08ebda1feb0777992f5536ea37a1b03
lui viton <artem_5511@mail.ru>
2022-09-24 20:27:02 +0300

    2week

---
+++ 8q.js
@@ -0,0 +1,7 @@
+//Return Negative
+//В этом простом задании вам дается число, и вы должны сделать его отрицательным. А может быть, число уже отрицательное?
+
+// function makeNegative(num) {
+//     if (num < 0) return num
+//     else return (num)*(-1)
+// }
