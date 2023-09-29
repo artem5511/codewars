e839b446f5a3df96788a9ea282d3c0d374494431
artem5511 <artem_5511@mail.ru>
2023-09-29 15:59:06 +0300

    Update 8q.js

--- 8q.js
+++ 8q.js
@@ -79,3 +79,10 @@ var summation = function (num) {
     }
     return sum
 }
+
+// Write a function "greet" that returns "hello world!"
+const greet = () => "hello world!";
+
+function greet() {
+    return "hello world!";
+    }
