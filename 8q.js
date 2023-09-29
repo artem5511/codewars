9274a2e783b45fda4c43cfe6612769813286c6ac
artem5511 <artem_5511@mail.ru>
2023-09-29 10:46:03 +0300

    Update 8q.js

--- 8q.js
+++ 8q.js
@@ -69,3 +69,13 @@ class SmallestIntegerFinder {
     return args.reduce((acc,el)=>acc < el ? acc : el)
   }
 }
+
+Summation
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+var summation = function (num) {
+    let sum = 0
+    for(let i = 0; i <= num; i++) {
+        sum += i
+    }
+    return sum
+}
