8ebcb8f398fb8241207265608c1452597605cfc0
artem5511 <artem_5511@mail.ru>
2023-11-02 00:39:20 +0300

    Update 8q.js

--- 8q.js
+++ 8q.js
@@ -143,3 +143,7 @@ function find_difference(a, b) {
 }
 
 To square(root) or not to square(root)
+
+function squareOrSquareRoot(array) {
+  return array.map(item => Math.sqrt(item) == Math.sqrt(item).toFixed(0) ? Math.sqrt(item) : item * item);  
+}
